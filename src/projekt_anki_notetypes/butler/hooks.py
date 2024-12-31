566579341e3febd3bc441ef94af77107ed6531ac
Jan <121321728+CravingCrates@users.noreply.github.com>
2024-12-31 16:26:08 +0100

    fix autocreate lernplan + make delete work on 'Hub

---
+++ src/projekt_anki_notetypes/butler/hooks.py
@@ -0,0 +1,61 @@
+
+from aqt import gui_hooks, mw
+from aqt.qt import *
+from anki import hooks
+import datetime
+from pathlib import Path
+from .lernplan_manager import create_filtered_deck
+
+ADDON_DIR_NAME = str(Path(__file__).parent.parent.name)
+
+
+def lernplan_auto_create():
+    # Set up the lernplan
+    # Check if the lernplan is already set up / config is available
+    conf = mw.addonManager.getConfig(ADDON_DIR_NAME)
+    if conf is not None and "lernplan" in conf:
+        lernplan_conf = conf["lernplan"]
+
+        # Check if the lernplan should be autocreated
+        if not lernplan_conf.get("autocreate", False):
+            return
+                
+        lerntag = int(lernplan_conf.get("lerntag", 1)) - 1
+        highyield = lernplan_conf.get("highyield", False)
+        lowyield = lernplan_conf.get("lowyield", False)
+
+        # Check if the lernplan is outdated
+        last_updated = datetime.datetime.fromisoformat(
+            lernplan_conf["last_updated"]
+        ).date()
+        today = datetime.datetime.today().date()
+        if not last_updated < today:
+            return # Lernplan is up to date
+
+        # Check if this weekday is in the list of weekdays
+        weekdays = lernplan_conf["wochentage"]
+        today_weekday = today.weekday()
+        if not weekdays[today_weekday]:
+            return # Today is not a lernplan day
+
+        # Increase the Lerntag
+        lerntag += 1
+        if lerntag > 85:
+            return  # Lernplan is finished
+
+        # Create the filtered deck
+        lernplan_conf["lerntag"] = str(lerntag).zfill(3)
+        lernplan_conf["last_updated"] = today.isoformat()
+        mw.addonManager.writeConfig(ADDON_DIR_NAME, conf)
+        create_filtered_deck(lerntag, highyield, lowyield)
+        print("Lernplan updated")
+
+    else:
+        # Lernplan is not set up
+        return None
+
+def onProfileLoaded():
+    lernplan_auto_create()
+
+def hooks_init():
+    gui_hooks.profile_did_open.append(onProfileLoaded)
